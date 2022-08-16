db0e3b54b55819caceb7646c33f1ec48676ed6b9
Niki Hite <nikihite@comcast.net>
2022-08-16 15:11:42 -0700

    shows list of users test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,12 +2,22 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
 
 const mockUser = {
   firstName: 'Niki',
   lastName: 'Hite',
   email: 'niki@hite.com',
   password: '12345',
+};
+
+const registerAndLogin = async (userProps = {}) => {
+  const password = userProps.password ?? mockUser.password;
+  const agent = request.agent(app);
+  const user = await UserService.create({ ...mockUser, ...userProps });
+  const { email } = user;
+  await agent.post('/api/v1/users/sessions').send({ email, password });
+  return [agent, user];
 };
 
 describe('backend-express-template routes', () => {
@@ -36,6 +46,12 @@ describe('backend-express-template routes', () => {
       .post('/api/v1/users/sessions')
       .send({ email: 'niki@hite.com', password: '12345' });
     expect(res.status).toEqual(200);
+  });
+
+  it('shows list of users', async () => {
+    const [agent] = await registerAndLogin();
+    const res = await agent.get('/api/v1/users');
+    expect(res.status).toBe(200);
   });
 
   afterAll(() => {
